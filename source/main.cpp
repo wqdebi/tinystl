ccea89c7b73da2755753fb28d9c05c2f3e013fe5
wqdebi <57791141+wqdebi@users.noreply.github.com>
2023-11-04 12:13:33 +0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -22,20 +22,42 @@
 //};
 
 //day3
-#include<exception>
-#include <iostream>
-//#include "std_construct.h"
-//#include"jjalloc.h"
-#include"std_alloc.h"
-#include<vector>
+//#include<exception>
+//#include <iostream>
+////#include "std_construct.h"
+////#include"jjalloc.h"
+//#include"std_alloc.h"
+//#include<vector>
+//int main()
+//{
+//    alloc x;
+//    //二级分配器
+//    auto* p= x.allocate(11);
+//    auto* pp = x.allocate(11);
+//    //一级分配器
+//    auto* ppp = x.allocate(114514);
+//    //A* p= new A[3];
+//    return 0;
+//}
+
+//day5
+#include<iostream>
+using namespace std;
+struct B {};
+struct D1: public B {};
+struct D2: public D1 {};
+template<typename I>
+void func(I& p, B) {
+    cout << "B" << endl;;
+}
+template<typename I>
+void func(I& p, D2) {
+    cout << "D2" << endl;
+}
 int main()
 {
-    alloc x;
-    //二级分配器
-    auto* p= x.allocate(11);
-    auto* pp = x.allocate(11);
-    //一级分配器
-    auto* ppp = x.allocate(114514);
-    //A* p= new A[3];
-    return 0;
+    int* p;
+    func(p, B());//调用void func(I& p, B)
+    func(p, D1());//调用void func(I & p, B)
+    func(p, D2());//调用void func(I& p, D2)
 }
