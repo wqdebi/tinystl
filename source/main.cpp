280e3913e5816060bd4998843d990603e3aca5c0
wqdebi <57791141+wqdebi@users.noreply.github.com>
2023-11-07 09:20:03 +0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -83,64 +83,85 @@
 //    func(p, D2());//调用void func(I& p, D2)
 //}
 
-//day6
-#include"stl_vector.h"
+////day6
+//#include"stl_vector.h"
+//#include<iostream>
+////using namespace std;
+//int main() {
+//    vector<int> a(5,5);
+//    std::cout << a[0] << std::endl;
+//    a.push_back(5);
+//    std::cout <<"a[0]:" << a[0] << std::endl;
+//    std::cout << "size:" << a.size() << std::endl;
+//    std::cout << "cap:" << a.capacity() << std::endl;
+//    a.push_back(*a.begin());
+//    for (int i = 0; i < a.size(); ++i)
+//    {
+//        std::cout << a[i] << " ";
+//    }
+//    std::cout << std::endl;
+//    a.pop_back();
+//    std::cout << "size:" << a.size() << std::endl;
+//    std::cout << "cap:" << a.capacity() << std::endl;
+//    std::cout << "empty:" << a.empty();
+//    std::cout << std::endl;
+//    a[2] = 114;
+//    for (int i = 0; i < a.size(); ++i)
+//    {
+//        std::cout << a[i] << " ";
+//    }
+//    std::cout << std::endl;
+//    auto it = a.end() - 1;
+//    a.insert(it, 2, 114);
+//    for (int i = 0; i < a.size(); ++i)
+//    {
+//        std::cout << a[i] << " ";
+//    }
+//    std::cout << std::endl;
+//    a.clear();
+//    for (int i = 0; i < a.size(); ++i)
+//    {
+//        std::cout << a[i] << " ";
+//    }
+//    vector<int> b;
+//    b.push_back(1);
+//    b.push_back(2);
+//    b.push_back(3);
+//    b.push_back(4);
+//    b.push_back(5);
+//    auto it0 = b.begin();
+//    auto it1 = b.begin() + 3;
+//    b.erase(it0, it1);
+//    for (int i = 0; i < b.size(); ++i)
+//    {
+//        std::cout << b[i] << " ";
+//    }
+//    std::cout << std::endl;
+//    std::cout << b.front() << std::endl;
+//    std::cout << b.back() << std::endl;
+//    //a.erase(it);
+//    //a.resize(3);
+//    //for (int i = 0; i < a.size(); ++i)
+//    //{
+//    //    std::cout << a[i] << " ";
+//    //}
+//    return 0;
+//}
+//day 8
 #include<iostream>
-//using namespace std;
-int main() {
-    vector<int> a(5,5);
-    std::cout << a[0] << std::endl;
-    a.push_back(5);
-    std::cout <<"a[0]:" << a[0] << std::endl;
-    std::cout << "size:" << a.size() << std::endl;
-    std::cout << "cap:" << a.capacity() << std::endl;
-    a.push_back(*a.begin());
-    for (int i = 0; i < a.size(); ++i)
-    {
-        std::cout << a[i] << " ";
-    }
-    std::cout << std::endl;
+#include"stl_list.h"
+int main()
+{
+    list<int> a;
+    a.push_back(8);
+    a.push_front(9);
+    a.push_back(8);
+    a.push_front(9);
     a.pop_back();
-    std::cout << "size:" << a.size() << std::endl;
-    std::cout << "cap:" << a.capacity() << std::endl;
-    std::cout << "empty:" << a.empty();
-    std::cout << std::endl;
-    a[2] = 114;
-    for (int i = 0; i < a.size(); ++i)
-    {
-        std::cout << a[i] << " ";
-    }
-    std::cout << std::endl;
-    auto it = a.end() - 1;
-    a.insert(it, 2, 114);
-    for (int i = 0; i < a.size(); ++i)
-    {
-        std::cout << a[i] << " ";
-    }
-    std::cout << std::endl;
-    a.clear();
-    for (int i = 0; i < a.size(); ++i)
-    {
-        std::cout << a[i] << " ";
-    }
-    vector<int> b;
-    b.push_back(1);
-    b.push_back(2);
-    b.push_back(3);
-    b.push_back(4);
-    b.push_back(5);
-    auto it0 = b.begin();
-    auto it1 = b.begin() + 3;
-    b.erase(it0, it1);
-    for (int i = 0; i < b.size(); ++i)
-    {
-        std::cout << b[i] << " ";
-    }
-    //a.erase(it);
-    //a.resize(3);
-    //for (int i = 0; i < a.size(); ++i)
+    a.pop_front();
+    std::cout << a.size();
+    //for (auto it1 = a.begin(), it2 = a.end(); it1 != it2; ++it1)
     //{
-    //    std::cout << a[i] << " ";
+    //    std::cout << *it1 << std::endl;
     //}
-    return 0;
 }
