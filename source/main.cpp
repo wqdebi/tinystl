309fca9bdf0af2d4af6e6fd5aad2718f31fbb371
wqdebi <57791141+wqdebi@users.noreply.github.com>
2023-11-30 21:30:37 +0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -205,35 +205,198 @@
     //}
 //}
 //day 10
-#include"stl_deque.h"
-#include<iostream>
+//#include"stl_deque.h"
+//#include<iostream>
+//int main() {
+//    deque<int> a;
+//    a.push_back(1);
+//    std::cout << a.back() << std::endl;
+//    a.push_back(2);
+//    std::cout << a.back() << std::endl;
+//    a.push_back(3);
+//    std::cout << a.back() << std::endl;
+
+    //deque<int> x(5, 3);
+    //for (auto it = x.begin(); it != x.end(); ++it)
+    //        std::cout << *it << " ";
+    //std::cout << std::endl;
+    //x.push_back(7);
+    //x[2] = 3;
+    //x[0] = 9;
+    //for (auto it = x.begin(); it != x.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    //x.pop_back();
+    //for (auto it = x.begin(); it != x.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    //x.pop_back();
+    //for (auto it = x.begin(); it != x.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    //x.pop_back();
+    //for (auto it = x.begin(); it != x.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    //x.pop_back();
+    //for (auto it = x.begin(); it != x.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    //x.pop_back();
+    //for (auto it = x.begin(); it != x.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    //x.pop_back();
+    //for (auto it = x.begin(); it != x.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    //deque<int> d(5,3);
+    //std::cout << d.size() << std::endl;
+    //std::cout << d[2] << std::endl;
+    //for (auto it = d.begin(); it != d.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    //deque<int> c;
+    //std::cout << c.empty() << std::endl;
+    //std::cout << c.size() << std::endl;
+    //for(int i = 0; i < 100; ++i)
+    //    c.push_back(114);
+    //std::cout << c.size() << std::endl;
+    //std::cout << c[90] << std::endl;
+    //c.pop_back();
+    //std::cout << c.size() << std::endl;
+    //c.push_front(5);
+    //std::cout << c[0] << std::endl;
+    //c.pop_front();
+    //std::cout << c[0] << std::endl;
+    //std::cout << c.front() << std::endl;
+    //c.push_front(5);
+    //c.push_front(5);
+    //c.push_back(1484);
+    //for (auto it = c.begin(); it != c.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+    ////c.erase(c.begin(), c.end());
+    ////std::cout << c.size() << std::endl;
+    //c[50] = 94;
+    //for (auto it = c.begin(); it != c.end(); ++it)
+    //    std::cout << *it << " ";
+    //std::cout << std::endl;
+//    return 0;
+//}
+
+//////day11
+//#include<iostream>
+//#include"stl_stack.h"
+//#include"stl_deque.h"
+//int main()
+//{
+//    //deque<int> x;
+//    //x.push_back(5);
+//    //x.push_back(5);
+//    //x.push_back(5);
+//    //x.push_back(5);
+//    //x.pop_back();
+//    //std::cout << x.size() << std::endl;
+//    //while (!x.empty())
+//    //{
+//    //    int z = x.size();
+//    //    std::cout << x.back() << " ";
+//    //    x.pop_back();
+//    //}
+//    stack<int> stk;
+//    stk.push(5);
+//    stk.push(5);
+//    stk.push(5);
+//    stk.push(5);
+//    stk.top() = 3;
+//    std::cout << stk.size() << std::endl;
+//    while (!stk.empty())
+//    {
+//        int x = stk.size();
+//        std::cout << stk.top() << " ";
+//        stk.pop();
+//    }
+//    std::cout << std::endl;
+//    return 0;
+//}
+
+//day12
+//template <class Arg, class Result> struct unary_function;
+//
+//template <class Arg, class Result>      // 1个参数，一个返回值
+//struct unary_function {
+//    typedef Arg argument_type;
+//    typedef Result result_type;
+//};
+//template<class T>
+//struct identity: public unary_function<T, T>
+//{
+//    const T& operator()(const T& x)const { return x; }
+//};
+//#include<iostream>
+//#include"rbtree.h"
+//#include<vector>
+//int main() {
+//    std::vector<int> x{ 5,4,1,7,8,0,2,3,9 };
+//    rb_tree<int, int, identity<int>, std::less<int>> rb;
+//    rb.insert_equal(x.begin(), x.end());
+//    std::cout << rb.size();
+//    return 0;
+//}
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include "rbtree.h"
+
+
+
+template <class T>
+struct identity
+{
+    identity() {}
+    const T& operator()(const T& val) {
+        return val;
+    }
+};
+
+using rep_type =
+rb_tree<int, int, identity<int>, std::less<int>>;
+
+void printTree(const rep_type& t) {
+    std::cout << "顺序输出结果：" << std::endl;
+    typename rep_type::const_iterator it = t.begin();
+    while (it != t.end()) {
+        std::cout << *it << " ";
+        ++it;
+    }
+    std::cout << std::endl;
+}
+
 int main() {
-    deque<int> d(5,3);
-    std::cout << d.size() << std::endl;
-    std::cout << d[2] << std::endl;
-    for (auto it = d.begin(); it != d.end(); ++it)
-        std::cout << *it << " ";
-    std::cout << std::endl;
-    deque<int> c;
-    std::cout << c.empty() << std::endl;
-    for(int i = 0; i < 100; ++i)
-        c.push_back(114);
-    std::cout << c.size() << std::endl;
-    std::cout << c[90] << std::endl;
-    c.pop_back();
-    std::cout << c.size() << std::endl;
-    c.push_front(5);
-    std::cout << c[0] << std::endl;
-    c.pop_front();
-    std::cout << c[0] << std::endl;
-    std::cout << c.front() << std::endl;
-    c.push_front(5);
-    c.push_front(5);
-    c.push_back(1484);
-    for (auto it = c.begin(); it != c.end(); ++it)
-        std::cout << *it << " ";
-    std::cout << std::endl;
-    c.erase(c.begin(), c.end());
-    std::cout << c.size() << std::endl;
+
+
+    rep_type t;
+    /*t.insert_unique(5);
+    t.insert_unique(6);
+    t.insert_unique(1);
+    t.insert_unique(0);
+    t.insert_unique(3);
+    t.insert_unique(2);
+    printTree(t);
+    t.erase(5);
+    t.erase(0);
+    printTree(t);*/
+    t.insert_equal(0);
+    t.insert_equal(1);
+    t.insert_equal(4);
+    t.insert_equal(0);
+    t.insert_equal(5);
+    t.insert_equal(8);
+    t.insert_equal(10);
+    printTree(t);
+    t.erase(0);
+    printTree(t);
     return 0;
 }
