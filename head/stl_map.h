8dd854b2853fb366e8a406022c7bc2e2e596c059
wqdebi <57791141+wqdebi@users.noreply.github.com>
2023-12-16 16:01:18 +0800

    Update stl_map.h

--- head/stl_map.h
+++ head/stl_map.h
@@ -2,30 +2,30 @@
 #include<algorithm>
 #include"std_alloc.h"
 #include"rbtree.h"
-
-template<class Arg, class Result>
-struct unary_function {
-	typedef Arg argument_type;
-	typedef Result result_type;
-};
-//template<class T>
-//struct identity : public unary_function<T, T> {
-//	const T& operator()(const T& x) { return x; }
+#include"stl_function.h"
+//template<class Arg, class Result>
+//struct unary_function {
+//	typedef Arg argument_type;
+//	typedef Result result_type;
 //};
-template<class Pair>
-struct select1st : public unary_function<Pair, typename Pair::first_type> {
-	const typename Pair::first_type& operator()(const Pair& x) {
-		return x.first;
-	}
-};
-
-template <class Arg1, class Arg2, class Result>
-struct binary_function {
-	typedef Arg1 first_argument_type;
-	typedef Arg2 second_argument_type;
-	typedef Result result_type;
-};
-template<class Key, class T,class Compare = std::less<Key>, class Alloc = alloc>
+////template<class T>
+////struct identity : public unary_function<T, T> {
+////	const T& operator()(const T& x) { return x; }
+////};
+//template<class Pair>
+//struct select1st : public unary_function<Pair, typename Pair::first_type> {
+//	const typename Pair::first_type& operator()(const Pair& x) {
+//		return x.first;
+//	}
+//};
+//
+//template <class Arg1, class Arg2, class Result>
+//struct binary_function {
+//	typedef Arg1 first_argument_type;
+//	typedef Arg2 second_argument_type;
+//	typedef Result result_type;
+//};
+template<class Key, class T, class Compare = std::less<Key>, class Alloc = alloc>
 class map {
 public:
 	using key_type = Key;
@@ -45,7 +45,7 @@ public:
 		}
 	};
 private:
-	using rep_type = rb_tree<key_type , value_type, select1st<value_type>, key_compare, Alloc>;
+	using rep_type = rb_tree<key_type, value_type, select1st<value_type>, key_compare, Alloc>;
 	rep_type t;
 
 public:
@@ -62,7 +62,7 @@ public:
 	explicit map(const Compare& comp) :t(comp) {}
 
 	template<class InputIterator>
-	map(InputIterator first, InputIterator last) :t(Compare()) {
+	map(InputIterator first, InputIterator last) : t(Compare()) {
 		t.insert_unique(first, last);
 	}
 	template<class InputIterator>
@@ -70,7 +70,7 @@ public:
 		t.insert_unique(first, last);
 	}
 	map(const map<Key, T, Compare, Alloc>& x) :t(x.t) {}
-	map<Key, T, Compare, Alloc>& operator=(const map<Key, T,Compare, Alloc>& x) {
+	map<Key, T, Compare, Alloc>& operator=(const map<Key, T, Compare, Alloc>& x) {
 		x = x.t;
 		return *this;
 	}
