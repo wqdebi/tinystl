64c1715a0c76ab8db23ff7aa399ded6e05bfcd23
wqdebi <57791141+wqdebi@users.noreply.github.com>
2023-12-16 16:01:59 +0800

    Update stl_multimap.h

--- head/stl_multimap.h
+++ head/stl_multimap.h
@@ -3,29 +3,30 @@
 #include<algorithm>
 #include"std_alloc.h"
 #include"rbtree.h"
+#include"stl_function.h"
 /////后续删除！！！！！！
-template<class Arg, class Result>
-struct unary_function {
-	typedef Arg argument_type;
-	typedef Result result_type;
-};
+//template<class Arg, class Result>
+//struct unary_function {
+//	typedef Arg argument_type;
+//	typedef Result result_type;
+//};
 //template<class T>
 //struct identity : public unary_function<T, T> {
 //	const T& operator()(const T& x) { return x; }
 //};
-template<class Pair>
-struct select1st : public unary_function<Pair, typename Pair::first_type> {
-	const typename Pair::first_type& operator()(const Pair& x) {
-		return x.first;
-	}
-};
+//template<class Pair>
+//struct select1st : public unary_function<Pair, typename Pair::first_type> {
+//	const typename Pair::first_type& operator()(const Pair& x) {
+//		return x.first;
+//	}
+//};
 
-template <class Arg1, class Arg2, class Result>
-struct binary_function {
-	typedef Arg1 first_argument_type;
-	typedef Arg2 second_argument_type;
-	typedef Result result_type;
-};
+//template <class Arg1, class Arg2, class Result>
+//struct binary_function {
+//	typedef Arg1 first_argument_type;
+//	typedef Arg2 second_argument_type;
+//	typedef Result result_type;
+//};
 
 template<class Key, class T, class Compare = std::less<Key>, class Alloc = alloc>
 class multimap {
